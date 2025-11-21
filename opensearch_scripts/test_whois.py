6ca9a641764375ae10a8f732da2709030d3f2735
Roimartinezr <roimartinezroque@gmail.com>
2025-11-21 14:37:34 +0100

    opensearch scripts

--- opensearch_scripts/test_whois.py
+++ opensearch_scripts/test_whois.py
@@ -1,6 +1,6 @@
 import whois
 
-w = whois.whois("abanca.com")
+w = whois.whois("xn--80adxhks.xn--p1ai")
 
 print(w)                     # imprime todo el objeto
 print("Country:", w.country) # a veces viene aquí
